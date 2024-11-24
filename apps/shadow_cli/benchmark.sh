3aa6d758af355a810284aefd3133600eea80640a
Jeremy Wildsmith <jeremyawildsmith@gmail.com>
2024-11-23 22:29:48 -0800

    Made node metrics configurable for benchmarking.

--- apps/shadow_cli/benchmark.sh
+++ apps/shadow_cli/benchmark.sh
@@ -3,12 +3,12 @@
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[-1]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 
-node_count="10,4,3,2,1"
+#node_count="10,4,3,2,1"
 
 algos="yescrypt,bcrypt,sha512crypt,sha256crypt,md5crypt"
 
 IFS=',' read -r -a algo_array <<< "$algos"
-IFS=',' read -r -a node_array <<< "$node_count"
+#IFS=',' read -r -a node_array <<< "$node_count"
 
 
 echo "==Network Configuration=="
@@ -17,9 +17,10 @@ read -p "  * Specify data-node fully qualified name: " data_node
 read -p "  * Specify data-node security cookie: " cookie_arg
 read -p "  * Specify local machine ip (must be accessible to data-node): " interface_name
 
+read -p "Optionally, specify the node counts to benchmark for in descending order [10,4,3,2,1]: " node_count
 read -p "Optionally, specify a custom password to use for benchmarking [tpw]: " benchmark_password
 
-if [ -z "$benchmark_password"]; then
+if [ -z "$benchmark_password" ]; then
     benchmark_password=tpw
 fi
 
@@ -35,6 +36,12 @@ if [ -n "$interface_name" ]; then
     interface_name="--interface $interface_name"
 fi
 
+if [ -z "$node_count" ]; then
+    node_count="10,4,3,2,1"
+fi
+
+IFS=',' read -r -a node_array <<< "$node_count"
+
 echo ""
 
 echo "Will submit jobs with the following command form:"
@@ -48,8 +55,8 @@ echo "Starting benchmark"
 
 mkdir -p $SCRIPT_DIRECTORY/benchmark
 
-#rm $SCRIPT_DIRECTORY/benchmark/*.dat &> /dev/null
-#rm $SCRIPT_DIRECTORY/benchmark/*.png &> /dev/null
+rm $SCRIPT_DIRECTORY/benchmark/*.dat &> /dev/null
+rm $SCRIPT_DIRECTORY/benchmark/*.png &> /dev/null
 
 
 for t in "${node_array[@]}"; do
@@ -58,8 +65,8 @@ for t in "${node_array[@]}"; do
 
     for algo in "${algo_array[@]}"; do
         echo "Benchmarking Node Performance for: $algo and $t nodes"
-        #a=$(mix shadow_cli submit --password $(mkpasswd -m $algo $benchmark_password) --get-results $data_node $cookie_arg $interface_name | sed -nE 's/.*Result :: .* :: ([0-9\.]+)s*/\1/p')
-        #echo $t $a >> $SCRIPT_DIRECTORY/benchmark/$algo.dat
+        a=$(mix shadow_cli submit --password $(mkpasswd -m $algo $benchmark_password) --get-results $data_node $cookie_arg $interface_name | sed -nE 's/.*Result :: .* :: ([0-9\.]+)s*/\1/p')
+        echo $t $a >> $SCRIPT_DIRECTORY/benchmark/$algo.dat
         echo "Done, sleeping to stabilize..."
         sleep 20
     done
